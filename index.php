66fc1592ce359d122d2b3b78e2441df904f656e9
David Rodriguez <jdavid.rodriguez.e@gmail.com>
2020-01-23 22:41:28 -0500

    Enlazando DB y creando crud

--- index.php
+++ index.php
@@ -5,8 +5,27 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
+require_once 'Task.php';
 
 use Relay\Relay;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'crud_php',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+  // Make this Capsule instance available globally via static methods... (optional)
+  $capsule->setAsGlobal();
+  // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+  $capsule->bootEloquent();
 
 $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER,
@@ -24,32 +43,52 @@ $twig = new \Twig_Environment($loader, array(
 
 $router = new Aura\Router\RouterContainer();
 $map = $router->getMap();
-$map->get('todo.list', '/', function ($request) use ($twig) {
-    $tasks = [
-        [
-            'id' => 1,
-            'description' => 'Aprender inglés',
-            'done' => false
-        ],
-        [
-            'id' => 1,
-            'description' => 'Hacer la tarea',
-            'done' => true
-        ],
-        [
-            'id' => 1,
-            'description' => 'Pasear al perro',
-            'done' => false
-        ],
-        [
-            'id' => 1,
-            'description' => 'Ver el curso de introducción a PHP',
-            'done' => false
-        ]
-    ];
+$map->get('crud_php.list', '/', function ($request) use ($twig) {
+    $tasks = Task::all();
     $response = new Zend\Diactoros\Response\HtmlResponse($twig->render('template.twig', [
         'tasks' => $tasks
     ]));
+    return $response;
+});
+
+$relay = new Relay([
+    new Middlewares\AuraRouter($router),
+    new Middlewares\RequestHandler()
+]);
+
+$map->post('crud_php.add', '/add', function ($request) {
+    $data = $request->getParsedBody();
+    $task = new Task();
+    $task->description = $data['description'];
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('crud_php.check', '/check/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    $task->done = true;
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('crud_php.uncheck', '/uncheck/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    $task->done = false;
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('crud_php.delete', '/delete/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    //$task->done = false;
+    $task->delete();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
     return $response;
 });
 
